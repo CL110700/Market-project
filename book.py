0dc73254a5e4bac69f319ed803fd99c2e07ea375
Cassandre Lefevre <cassandre.lefevre06250@gmail.com>
2022-03-24 18:43:48 +0100

    creation class book et fonction init et insert buy

---
+++ book.py
@@ -0,0 +1,21 @@
+from enum import Enum
+from texttable import Texttable
+
+class Book:
+    def __init__(self,name):
+        self._name=name
+        self._buy=[]
+        self._sell=[]
+    def insert_buy(self,quantity,price):
+        a=Order(Side.BUY,quantity,price) #order creation
+        self._buy.append(a)
+        self._buy=sorted(self._buy,key=lambda order: -order.price)
+        #Print detail
+        print(f"--- Insert {a.side.value} {a.quantity}@{a.price} on {self.name}")
+        #Check Execution
+        while self.Check_execution():
+            self.Execution()
+        #print book
+        print (self.UI())
+
+
